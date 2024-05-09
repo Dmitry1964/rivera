43540b29413cc02f4b7d196c85aa41a46918b45b
Dmytry64 <nosdm64@gmail.com>
2024-05-09 15:25:02 +0300

    feat добавляет баннер

--- source/js/main.js
+++ source/js/main.js
@@ -1,8 +1,3 @@
-import {mobileVhFix} from './utils/mobile-vh-fix.js';
-import {initModals} from './modules/modals/init-modals';
-import {Form} from './modules/form-validate/form';
-import {CustomSelect} from './modules/select/custom-select';
-import {uploadFile, uploadImageDrop} from './modules/input-file/init-upload';
 
 // ---------------------------------
 
@@ -11,7 +6,6 @@ window.addEventListener('DOMContentLoaded', () => {
   // Utils
   // ---------------------------------
 
-  mobileVhFix();
 
   // Modules
   // ---------------------------------
@@ -19,14 +13,6 @@ window.addEventListener('DOMContentLoaded', () => {
   // все скрипты должны быть в обработчике 'DOMContentLoaded', но не все в 'load'
   // в load следует добавить скрипты, не участвующие в работе первого экрана
   window.addEventListener('load', () => {
-    initModals();
-    uploadFile();
-    uploadImageDrop();
-    const select = new CustomSelect();
-    select.init();
-    const form = new Form();
-    window.form = form;
-    form.init();
   });
 });
 
